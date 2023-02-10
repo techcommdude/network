3f9110a2d69dd7154307304a511f93e9062e80c6
GF <farnellg@gmail.com>
2023-02-10 16:24:10 -0500

    asdf

--- network/views.py
+++ network/views.py
@@ -294,7 +294,7 @@ def djangoAllPosts(request):
     # Pagination details.
     postsPagination = postings
     # Show this many postings per page.
-    paginator = Paginator(postsPagination, 2)
+    paginator = Paginator(postsPagination, 5)
 
     page_number = request.GET.get('page', '1')
     page_obj = paginator.get_page(page_number)
